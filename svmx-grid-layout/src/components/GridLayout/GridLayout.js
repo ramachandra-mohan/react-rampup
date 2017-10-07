e74edeaece36eb02b8dc5e876a43be942fe16eef
Ramachandra Mohan <ramachandra.mohan@servicemax.com>
2017-10-07 10:29:32 +0530

    Added comments and logs

--- svmx-grid-layout/src/components/GridLayout/GridLayout.js
+++ svmx-grid-layout/src/components/GridLayout/GridLayout.js
@@ -1,4 +1,14 @@
 // @flow
+/**
+ * The SVMX UI Layout module defines the 'grid layout' for SVMX UI.
+ * Layout items can be autosized, or given a specific width, and layouts
+ * reflow responsively on smaller viewports.
+ *
+ * Eg.
+ * <GridLayout gutters='large' alignment='center' vertical='middle'>
+ *  <Layout />
+ * </GridLayout>
+ */
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
@@ -6,30 +16,63 @@ import './GridLayout.css';
 
 class GridLayout extends PureComponent {
 
-  static _gutters = { regular : '', flush : 'layout--flush', tiny : 'layout--tiny', small : 'layout--small', large : 'layout--large', huge : 'layout--huge' };
+  /**
+   * Static variable to hold supported gutters.
+   */
+  static _gutters = { regular : '', flush : 'layout--flush', tiny : 'layout--tiny', small : 'layout--small', large : 'layout--large', huge : 'layout--huge'};
+
+  /**
+   * Static variable to hold supported alignments.
+   */
   static _alignments = { left: '', center : 'layout--center', right : 'layout--right' };
+
+  /**
+   * Static variable to hold supported verticals.
+   */
   static _verticals = { top: '', middle : 'layout--middle', bottom : 'layout--bottom', full : 'layout--full' };
 
+  /**
+   * Class constructor to create instance of this class with state.
+   */
   constructor(props) {
     super(props);
 
+    console.debug( 'GridLayout::constructor(props) : props=',  props );
+
+    /*
+     * Initializing state with gutter, alignment and vertical properties.
+     */
     this.state = {
-      gutter: props.gutter,
-      alignment: props.alignment,
-      vertical: props.vertical
+      gutter: props.gutter, alignment: props.alignment, vertical: props.vertical
     }
+
+    console.info( 'GridLayout::constructor(props) : state=', this.state );
   }
 
+  /**
+   * This method is used to retrieve style component for this grid layout.
+   */
   get _style() {
+
+    console.debug( 'GridLayout::_style() - enter' );
 
     let gutter = GridLayout._gutters[this.state.gutter];
     let vertical = GridLayout._verticals[this.state.vertical];
     let alignment = GridLayout._alignments[this.state.alignment];
 
-    return `layout ${alignment} ${vertical} ${gutter}`.trim();
+    let style = `layout ${alignment} ${vertical} ${gutter}`.trim();
+
+    console.info( 'GridLayout::_style() : style=', style );
+    console.debug( 'GridLayout::_style() - exit' );
+
+    return style;
   }
 
+  /**
+   * Render method to return grid layout component with configured style.
+   */
   render() {
+    console.debug( 'GridLayout::render()' );
     return (
       <div className={this._style}>
         {this.props.children}
@@ -38,12 +81,16 @@ class GridLayout extends PureComponent {
   }
 }
 
+/**
+ * Default props values for gutter, alignment and vertical attributes.
+ */
 GridLayout.defaultProps = {
-  gutter: 'regular',
-  alignment: 'left',
-  vertical: 'top',
+  gutter: 'regular', alignment: 'left', vertical: 'top'
 }
 
+/**
+ * Prop types and list of supported values.
+ */
 GridLayout.propTypes = {
   gutter: PropTypes.oneOf(['regular', 'flush', 'tiny', 'small', 'large', 'huge']),
   alignment: PropTypes.oneOf(['left', 'center', 'right']),
