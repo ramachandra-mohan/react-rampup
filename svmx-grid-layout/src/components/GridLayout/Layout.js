865f65610850e536f0574b05321fe1087e205d7c
Ramachandra Mohan <ramachandra.mohan@servicemax.com>
2017-10-09 09:16:48 +0530

    Updated layout to add default values and comments

--- svmx-grid-layout/src/components/GridLayout/Layout.js
+++ svmx-grid-layout/src/components/GridLayout/Layout.js
@@ -1,26 +1,46 @@
 // @flow
+/**
+ * The SVMX UI Layout module defines the 'layout' for SVMX UI.
+ *
+ * Eg.
+ * <Layout verticals='middle' width='1/3'/>
+ */
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 class Layout extends PureComponent {
 
+  /**
+   * Static variable to hold supported verticals.
+   */
   static _verticals = { top: '', middle : 'layout--middle', bottom : 'layout--bottom', full : 'layout--full' };
 
+  /**
+   * Class constructor to create instance of this class with state.
+   */
   constructor(props) {
     super(props);
 
+    /*
+     * Initializing state with width and vertical properties.
+     */
     this.state = {
-      width: props.width || '1/3',
-      vertical: props.vertical || 'top'
+      width: props.width, vertical: props.vertical
     }
   }
 
+  /**
+   * This method is used to retrieve style component for this grid layout.
+   */
   get _style() {
 
     let vertical = Layout._verticals[this.state.vertical];
     return `layout__item u-${this.state.width} ${vertical}`.trim();
   }
 
+  /**
+   * Render method to return layout component with configured style.
+   */
   render() {
     return (
       <div className={this._style}>
@@ -30,6 +50,16 @@ class Layout extends PureComponent {
   }
 }
 
+/**
+ * Default props values for gutter, alignment and vertical attributes.
+ */
+Layout.defaultProps = {
+  vertical: 'top', width: '1/3'
+}
+
+/**
+ * Prop types and list of supported values.
+ */
 Layout.propTypes = {
   width: PropTypes.string.isRequired,
   vertical: PropTypes.oneOf(['top', 'middle', 'bottom', 'full']),
