745f09b53544c272531dd9a5cc8f556d75b44750
Ramachandra Mohan <ramachandra.mohan@servicemax.com>
2017-10-10 07:50:26 +0530

    multiple component export fix

--- svmx-ui-components/src/components/GridLayout/index.js
+++ svmx-ui-components/src/components/GridLayout/index.js
@@ -1,3 +1,10 @@
+/**
+ * The SVMX UI Layout module exports the 'grid layout' for SVMX UI. One can import
+ * the components as below.
+ *
+ * Eg.
+ *  import {GridLayout, Layout} from 'components/GridLayout'
+ */
 import GridLayout from './GridLayout';
 import Layout from './Layout';
-export default { GridLayout, Layout };
+export { GridLayout, Layout };
